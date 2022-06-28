737f5bce5eb7d823da4e320a9719ebf330b17def
Francisco Gonzalez <fco.gonzalezvz@gmail.com>
2022-06-27 20:55:11 -0400

    car store desafio latam

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const boton = document.getElementById("toggle");
+// const nav = document.query
+// console.log(boton);
+
+boton.addEventListener("click", () => {
+  document.getElementById("header__nav").classList.toggle("show");
+  });
+
+
+  // document.getElementById("toggle").addEventListener("click", () => {
+  //   document.getElementById("header__nav").classList.toggle("show");
+  // });
+  
+  
